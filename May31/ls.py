5db6fdeceeb3fb8ccb4770a2e02387ef90d80a82
trph40 <tphind40@proton.me>
2025-05-31 21:51:36 +0500

    as

---
+++ May31/ls.py
@@ -0,0 +1,36 @@
+﻿# def deco(func):
+#     def wrapper(*args):
+#         result = []
+#         for i in args:
+#             if i > 0:
+#                 result.append(i)
+#             else:
+#                 result.append(False)
+#         func(result)
+#     return wrapper
+#
+# @deco
+# def salom(a):
+#     print(a)
+#
+# salom(-2, 4, 9, -1)
+
+
+
+users = ['shaxriyor6', 'javohir456', "fefe2342", "fewfs"]
+
+def deco(func):
+    def wrapper(username):
+        if username in users:
+            func(username)
+        else:
+            print("Username not found")
+    return wrapper
+
+
+@deco
+def funk(username):
+    print("Hello " + username)
+
+
+funk("shaxriyor61")
